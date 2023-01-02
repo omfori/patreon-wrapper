d86f035d3e70ddd0e59ae8865bd24d346f593c0d
Jedd <38686545+jedddg@users.noreply.github.com>
2023-01-02 21:33:54 +1100

    fix types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,8 @@
-declare type Auth = {
+export type Auth = {
     AccessToken: string;
     CampaignID: string;
 };
-declare type PatronType = {
+export type PatronType = {
     displayId: string;
     displayName: string;
     emailAddress: string;
@@ -10,7 +10,7 @@ declare type PatronType = {
     subscription: {
         note: string;
         currentEntitled: {
-            status: 'active_patron' | 'declined_patron';
+            status: "active_patron" | "declined_patron";
             tier: {
                 id: string;
                 title: string;
@@ -34,7 +34,7 @@ declare type PatronType = {
         };
     };
 };
-declare type SandboxOptions = {
+export type SandboxOptions = {
     displayId: string;
     displayName: string;
     emailAddress: string;
@@ -45,7 +45,7 @@ declare type SandboxOptions = {
     cents: number;
     willPayCents: number;
     lifetimeCents: number;
-    patronStatus: 'active_patron' | 'declined_patron';
+    patronStatus: "active_patron" | "declined_patron";
     firstCharge: string;
     nextCharge: string;
     lastCharge: string;
@@ -68,7 +68,7 @@ export declare class Patreon {
     static Authorization(AuthInformation: Auth): void;
     private static FetchAPI;
     private static CleanQueryURL;
-    static FetchPatrons(filters?: Array<'active_patron' | 'declined_patron'>, pageSize?: number): Promise<PatronType[]>;
+    static FetchPatrons(filters?: Array<"active_patron" | "declined_patron">, pageSize?: number): Promise<PatronType[]>;
     protected static _SandboxAdd(Patron: SandboxOptions): void;
     protected static _SandboxGet(): SandboxOptions[];
 }
@@ -76,5 +76,4 @@ export declare class Sandbox extends Patreon {
     static GetFakePatrons(): SandboxOptions[];
     static AppendPatron(Patron: SandboxOptions): void;
 }
-export {};
 //# sourceMappingURL=index.d.ts.map
